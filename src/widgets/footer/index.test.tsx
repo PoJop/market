cb57b45a9843a1827d535bc8bf0c1ecc1d789af8
Oleg <kostetzkiy.oleg@gmail.com>
2023-02-04 21:34:34 +0200

    correction

--- src/widgets/footer/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import { Footer } from './index'
-import '@testing-library/jest-dom'
-
-describe('Footer', () => {
-    it('renders a footer', () => {
-        render(<Footer />)
-    })
-})
--- src/widgets/header/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import { Header } from './index'
-import '@testing-library/jest-dom'
-
-describe('Header', () => {
-    it('renders a header', () => {
-        render(<Header />)
-    })
-})
